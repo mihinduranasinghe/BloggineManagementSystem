533ba0271befddafdbbafab03a70b684705f7a2a
Mihindu Ranasinghe <37303081+MihinduRanasinghe@users.noreply.github.com>
2019-07-16 14:53:39 +0530

    dsiplaying only 200 letters of every post in index page

--- cms/admin/includes/admin_navigation.php
+++ cms/admin/includes/admin_navigation.php
@@ -7,11 +7,11 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin</a>
+                <a class="navbar-brand" href="index.html">Blog Management System Admin</a>
             </div>
             <!-- Top Menu Items -->
             <ul class="nav navbar-right top-nav">
-               <li><a href="../index.php">Home Page</a></li>
+               <li><a href="../index.php">Home Site</a></li>
                 
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> John Smith <b class="caret"></b></a>
